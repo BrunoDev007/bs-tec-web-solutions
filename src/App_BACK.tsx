a986e22ea89772a5978d55d3729d0f1c963c1164
BrunoDev007 <bruno_xr@outlook.com>
2025-06-28 21:44:43 -0300

    Novas alteraçoes Vite e App

---
+++ src/App_BACK.tsx
@@ -0,0 +1,47 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import WhatsAppButton from "./components/WhatsAppButton";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Support from "./pages/Support";
+import Contact from "./pages/Contact";
+import Arquivos from "./pages/Arquivos";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter basename="/bs-tec-web-solutions">
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/servicos" element={<Services />} />
+              <Route path="/quem-somos" element={<About />} />
+              <Route path="/suporte" element={<Support />} />
+              <Route path="/contato" element={<Contact />} />
+              <Route path="/arquivos" element={<Arquivos />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+          <WhatsAppButton />
+        </div>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
