d7cc0299b8b45f4acdfdc3c596c3a6d1914cee9b
BrunoDev007 <bruno_xr@outlook.com>
2025-06-29 11:15:33 -0300

    Mudança do App.tsx

--- src/App_Back3.tsx
+++ src/App_Back3.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom"; // alterado aqui
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import WhatsAppButton from "./components/WhatsAppButton";
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter> {/* alterado aqui */}
+      <BrowserRouter basename="/bs-tec-web-solutions">
         <div className="min-h-screen flex flex-col">
           <Navbar />
           <main className="flex-grow">
@@ -38,7 +38,7 @@ const App = () => (
           <Footer />
           <WhatsAppButton />
         </div>
-      </HashRouter>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
